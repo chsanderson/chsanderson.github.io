401293fcb220c496917814f5dafb138314a81cd1
ChSan <chsanderson@live.co.uk>
2022-06-28 11:21:11 +0100

    Update Website

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-$(document).ready(){
-    const scrollUp = $(document).getElementById("#scroll-up")
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    {
+        document.getElementById("scroll-up").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("scroll-up").style.display = "none";
+    }
+}
+function topFunction() 
+{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
