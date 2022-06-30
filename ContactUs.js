574bba4d769d1c6d5ea79f292c1d1245fe21c981
ChSan <chsanderson@live.co.uk>
2022-06-30 02:55:23 +0100

    Layout Commit

---
+++ ContactUs.js
@@ -0,0 +1,13 @@
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        
+    }
+
+}
--- index.js
+++ index.js
@@ -15,3 +15,7 @@ function topFunction()
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+//function email
+
+window.onload
